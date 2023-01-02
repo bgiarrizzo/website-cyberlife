101810633645c1acd9102474c2ffd1afc212d597
Bruno Giarrizzo <b.giarrizzo@cyberlife.sh>
2023-01-02 18:11:32 +0100

    add reveal effect

--- js/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('tsparticles', 'assets/particles.json', function() {
-  console.log('callback - particles config loaded');
-});
